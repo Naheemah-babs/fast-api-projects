0729375b00b8018133f60d109419d76ba160b21d
naheemah-babs <babatundenaheemot@gmail.com>
2026-07-17 18:07:07 +0100

    created the update to do endpoint iin main.py

--- fast-api-class-3/main.py
+++ fast-api-class-3/main.py
@@ -29,3 +29,25 @@ def create_todo(payload:TodoCreate, db: Session = Depends(get_db)):
 )
 def list_todos(db: Session = Depends(get_db)):
     return db.query(Todo).all()
+
+#read one
+@app.get("/todos/{todo_id}", response_model=TodoResponse, status_code=status.HTTP_200_OK)
+def list_one_todo(todo_id:int, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Todo with id {todo_id} not found")
+    return todo
+
+#update todo
+@app.put("/todos/{todo_id}", response_model=TodoResponse, status_code=status.HTTP_200_OK)
+def update_todo(todo_id:int, payload: TodoUpdate, db: Session = Depends(get_db)):
+    todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Todo with id {todo_id} not found")
+    
+    for field, value in payload.model_dump(exclude_unset=True).items():
+        setattr(todo, field, value)
+
+    db.commit()
+    db.refresh(todo)
+    return todo
